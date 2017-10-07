30ecb1753f8bcbb841a624c224368b6b31e11892
hidehiro okamoto <okagen@uchida.co.jp>
2017-10-07 09:30:00 +0900

    Amend something.

--- b02-FlickrAPI.py
+++ b02-FlickrAPI.py
@@ -1,52 +1,54 @@
 # -*- coding: utf-8 -*-
 """
-Flickr
+Get some pictures from Flickr.
 """
 from flickrapi import FlickrAPI
 from urllib.request import urlretrieve
 from pprint import pprint
-import os, time, sys
+import os, time
 
 # API Keyの設定
 key = ""
 secret = ""
- 
-# ダウンロード後の待機時間
-wait_time = 1
 
-# キーワードをチェックする
-#if len(sys.argv) < 2:
-#    print("python dl.py (keyword)")
-#    sys.exit()
-
-#keyword = sys.argv[1]
+# 検索キーワード
 keyword = "cat"
+
+# 検索件数＝ダウンロード数
+countPict = 500
+
+# 画像保存先のディレクトリ 無ければ作る
 savedir = "./" + keyword
 if not os.path.exists(savedir):
     os.mkdir(savedir)
 
-# Flickr APIで写真を検索
+# Flickr写真を検索 結果をJSONで取得
 flickr = FlickrAPI(key, secret, format="parsed-json")
 res = flickr.photos.search(
     text = keyword,
-    per_page = 500,
+    per_page = countPict,
     media = "photos",
     sort = "relevance",
     safe_search = 1,
     extras = "url_q, license")
-pprint(res)
 
 # 検索結果を確認
 photos = res["photos"]
 pprint(photos)
+
+# ダウンロード後の待機時間
+wait_time = 1
+
+# 画像をダウンロード
 try:
-    # 1つずつ画像をダウンロードする
     for i , photo in enumerate(photos["photo"]):
         url_q = photo["url_q"]
         filepath = savedir + "/" + photo["id"] + ".jpg"
         if os.path.exists(filepath): continue
-        print(str(i + 1) + ":download=", url_q)
+
         urlretrieve(url_q, filepath)
+        print(str(i + 1) + ":download=", url_q)
+
         time.sleep(wait_time)
 except:
     import traceback
